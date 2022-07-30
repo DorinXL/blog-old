5f93d26b76e4ab1a481564f9c386f48f37646298
DorinXL <974781841@qq.com>
2022-07-30 23:19:22 +0800

    准备存放unity的demo

--- js/sidebar.js
+++
@@ -1,8 +0,0 @@
-/*关于我们侧边栏弹出*/
-document.querySelector('#aboutus').addEventListener('click', function () {
-	document.querySelector('#sidebar').style.right = '0';
-}, false);
-
-document.querySelector('#closebar').addEventListener('click', function () {
-	document.querySelector('#sidebar').style.right = '-300px';
-}, false);
